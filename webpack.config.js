77ebe2784e7677c168d9b6804b07ba67bc08444b
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2021-11-02 20:31:22 +0200

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin= require("css-minimizer-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./front/index.js",
+    output: {
+        filename: "app.js",
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "./"
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./front/index.html"
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sacc-loader",
+                ]
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: "asset/resource",
+            },
+        ]
+    },
+    optimization:{
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin()
+        ]
+    }
+}
