bb8a7dc86b1c489ac318fc42c9340f7be0ad0289
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2021-11-06 15:07:19 +0200

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const linkRouter = require("./routers/linkRouter.js");
+const linkRouter = require("./back/routers/linkRouter.js");
 
 const app = express();
 app.use(express.json());
