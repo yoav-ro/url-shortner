1b316778e78668c69d6c28911a7558d4a94c2107
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2021-11-06 14:42:38 +0200

    added procfile

--- back/index.js
+++ back/index.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/link", linkRouter)
+
+app.use('/', express.static(path.resolve('./dist'))); // serve main path as static dir
+app.get('/', (req, res) => { // serve main path as static file
+    res.sendFile(path.resolve('./dist/index.html'))
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
